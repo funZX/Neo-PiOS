6a92094e59c470000b300d1c95d36639110fd3a4
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-08-23 00:42:10 +0200

    Rename layer from meta-crankos to meta-neopios

--- build/env-crank-os.sh
+++ build/env-crank-os.sh
@@ -5,7 +5,7 @@ alias bb=${BB_COMMAND}
 
 alias bb.clean="bitbake -c clean -c cleansstate"
 alias bb.edit="devtool modify"
-alias bb.apply="devtool update-recipe -a ../layers/meta-crankos"
+alias bb.apply="devtool update-recipe -a ../layers/meta-neopios"
 alias bb.close="devtool reset"
 alias bb.shell="bitbake -c devshell"
 alias bb.linux="bitbake linux -c"
